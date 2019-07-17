0f746d38ef1c7e79d3ce94ef3d3f65e01ee9fe84
Chris Adams <chris@productscience.co.uk>
2019-07-17 22:24:49 +0200

    Add token test

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+
+import debugLib from "debug"
+import { exportAllDeclaration } from "@babel/types";
+const debug = debugLib("tgwf:test:greenHouse")
+
+describe("Greenhouse", () => {
+  test("empty test to fill in later", () => {
+    expect(1 + 1).toEqual(2)
+  })
+})
